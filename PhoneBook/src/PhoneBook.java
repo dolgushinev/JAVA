2caff53254d1de729ace3e10db95cab80db33e53
Evgeny_D <dolgushinev@mail.ru>
2020-07-02 18:57:54 +0300

    1) Доработан метод formatPhoneNumber. Результирующая строка теперь хранится в переменной result 2) Разработан метод add

--- PhoneBook/src/PhoneBook.java
+++ PhoneBook/src/PhoneBook.java
@@ -1,14 +1,31 @@
+import java.util.Arrays;
+
 public class PhoneBook {
+
+    private static String[][] book = new String[3][3];
+    private static int nextIndex = 0;
 
     public static void main(String[] args) {
         //Добавить считывание ввода пользователя в цикле
-        String FIO = "Иван Иванович иванов";
+/*        String FIO = "Иван Иванович иванов";
         String phone = "8(926)280-03-03";
         System.out.println("checkName(FIO): " + checkName(FIO));
         System.out.println("formatName(FIO): " + formatName(FIO));
         System.out.println("checkPhoneNumber(phone): " + checkPhoneNumber(phone));
-        phone = formatPhoneNumber(phone);
-        System.out.println(phone);
+        System.out.println("formatPhoneNumber(phone): " + formatPhoneNumber(phone));*/
+
+        System.out.println("Первый вызов add");
+        add(book, formatName("Иванов иван Иванович"), formatPhoneNumber("8(926)280-03-03"));
+        System.out.println("Второй вызов add");
+        add(book, formatName("Петров Иван иванович"), formatPhoneNumber("8(926)111-11-11"));
+        System.out.println("Третий вызов add");
+        add(book, formatName("сидоров Иван Иванович"), formatPhoneNumber("8(926)280-03-03"));
+
+        System.out.println("Итого: ");
+        for (int i = 0; i < nextIndex; i++) {
+            System.out.println(book[i][0] + " " + book[i][1]);
+        }
+        System.out.println("");
     }
 
     public static boolean checkName(String name) {
@@ -38,14 +55,39 @@ public class PhoneBook {
     }
 
     public static String formatPhoneNumber(String number) {
-        number = number.replaceAll("[^0-9]", "");
-        number = "+7" + " " + number.substring(1, 4) + " " +
-                number.substring(4, 7) + " " + number.substring(7, 9) + " " + number.substring(9);
-        return number;
+        String clean = number.replaceAll("[^0-9]", "");
+        String result = "+7" + " " + clean.substring(1, 4) + " " +
+                clean.substring(4, 7) + " " + clean.substring(7, 9) + " " + clean.substring(9);
+        return result;
     }
 
+/*    public static void add(String[][] book, String name, String number) {
+        boolean nameIsExist = false;
+        int indexOfCopy = -1;
+        for (int i = 0; i < nextIndex; i++) {
+            if (book[i][0].equals(name)) {
+                System.out.println(book[i][0] + " " + book[i][1]);
+                indexOfCopy = i;
+                nameIsExist = true;
+                break;
+            }
+        }
+        if (!nameIsExist) {
+            book[nextIndex][0] = name;
+            book[nextIndex][1] = number;
+            nextIndex++;
+        } else System.out.println("ФИО присутствует в справочнике, телефон: " + book[indexOfCopy][1]);
+
+        System.out.println("Итого: ");
+        for (int i = 0; i < nextIndex; i++) {
+            System.out.println(book[i][0] + " " + book[i][1]);
+        }
+        System.out.println("");*/
+
     public static void add(String[][] book, String name, String number) {
-        //add logic
+            book[nextIndex][0] = name;
+            book[nextIndex][1] = number;
+            nextIndex++;
     }
 
     public static void list(String[][] book) {
