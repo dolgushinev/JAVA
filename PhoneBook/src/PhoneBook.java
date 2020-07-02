9763afe1ca0f76d84377db6ad6bcd9251502f27a
Evgeny_D <dolgushinev@mail.ru>
2020-07-02 15:25:32 +0300

    Реализован метод formatName

--- PhoneBook/src/PhoneBook.java
+++ PhoneBook/src/PhoneBook.java
@@ -3,7 +3,7 @@ public class PhoneBook {
     public static void main(String[] args) {
         //Добавить считывание ввода пользователя в цикле
         System.out.println(checkName("Иван Иванович Иванов"));
-
+        System.out.println(formatName("   иванов Иван иванович    "));
     }
 
     public static boolean checkPhoneNumber(String phoneNumber) {
@@ -12,11 +12,23 @@ public class PhoneBook {
 
     public static boolean checkName(String name) {
         String[] words = name.trim().split(" ");
+        System.out.println(words.length);
         return words.length == 3;
     }
 
     public static String formatName(String name) {
-        return "";
+        String[] words = name.trim().split(" ");
+        String result = "";
+        for (int i = 0; i < words.length; i++) {
+            String str = words[i];
+            char firstChar = str.charAt(0);
+            if (!Character.isUpperCase(firstChar)) {
+                result += Character.toUpperCase(firstChar) + str.substring(1) + " ";
+            } else {
+                result += str + " ";
+            }
+        }
+        return result;
     }
 
     public static String formatPhoneNumber(String number) {
